5c7bead179dd80d7e5a1e8055b23d0f46348ddea
MangArya11 <komanggede86@gmail.com>
2020-12-24 23:15:13 +0800

    delete vs folder

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ while True:
     detect_img = img_resize.reshape(-1, 300, 160, 1)
 
     prediction = model.predict(detect_img)
+    # coor = model.detectMultiScaleI(detect_img)
 
     name = CATEGORIES[int(prediction[0][0])]
     
-    # for (x, y, w, h) in prediction:
+    # for (x, y, w, h) in coor:
     #     cv2.rectangle(frame,(x, y),(x+w, y+h),(0,255,0), 2)
-    cv2.putText(gray_scale,name,(10,20),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,0),2)
+    scv2.putText(gray_scale,name,(10,20),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,0),2)
 
     cv2.imshow("Video",gray_scale)
 
